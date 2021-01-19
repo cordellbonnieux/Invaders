dfa9dd652039dbb3bc14b69ce04dcc82466d1a0a
Cordell <cordellbonnieux@gmail.com>
2021-01-18 18:49:29 -0800

    Created a player who can move back and forth!

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
-
-const gameArea = document.getElementById('gameArea')
-const player = document.createElement('div')
-
-function buildGameArea(gameArea){
-
-}
 import css from './style.css'
+const gameArea = document.getElementById('gameArea')
+const player = document.getElementById('player')
+
+// center the player on start
+player.style.cssText = 'left:230px;'
+
+function addControls(player){
+    document.addEventListener('keyup', (event) => {
+        let pos = window.getComputedStyle(player)
+        let left = parseFloat(pos.left)
+        let right = parseFloat(pos.right)
+        if (event.key == 'a'){
+            left -= 10
+            left.toString()
+            if (left > 0){
+                player.style.cssText = `left:${left}px`
+            } else {
+                return
+            }
+        }
+        if (event.key == 'd'){
+            right -= 10
+            right.toString()
+            if (right > 0){
+                player.style.cssText = `right:${right}px`
+            } else {
+                return
+            }
+        }
+    }) 
+}
+addControls(player)
